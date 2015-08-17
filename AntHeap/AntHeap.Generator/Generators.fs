da6d3ecec3a126f7869319b69dd5eda43a2962b2
Serg Rogovtsev <lair@hawke.ru>
2015-08-17 23:12:36 +0300

    Finished generator

---
+++ AntHeap/AntHeap.Generator/Generators.fs
@@ -0,0 +1,48 @@
+﻿module Generators
+
+open System
+open System.IO
+
+let rand = Random()
+
+let generateAnts bound writer =
+    let antsByType =
+        seq {0..255}
+        |> Seq.mapFold (fun s i -> 
+            let q =
+                //randomly chosen
+                match rand.Next(0, bound/256*2) with
+                | v when v >= 0 -> v
+                | _ -> 0
+            
+            if i = 255 then
+                (s, 0)
+            elif (s < q) then
+                (s, 0)
+            else
+                (q, s-q)
+        ) bound
+        |> fst
+        |> Seq.map (fun i -> Array.init i (fun _ -> Guid.NewGuid()))
+        |> Seq.toArray
+
+    writer (fun w ->
+        antsByType
+        |> Array.iteri (fun t ids ->
+            ids |> Array.iter (fun id -> w id (t |> byte))
+        )
+    )
+
+    antsByType |> Seq.concat
+
+let generateCells bound writer = 
+    let cells = Array.init bound (fun _ -> Guid.NewGuid())
+    
+    writer (fun w ->
+        cells
+        |> Seq.sort
+        |> Seq.iter (fun id -> w id (rand.Next(0, 256) |> byte))
+    )
+
+    cells
+
--- AntHeap/AntHeap.Generator/Program.fs
+++ AntHeap/AntHeap.Generator/Program.fs
@@ -1,7 +1,46 @@
 ﻿open System
+open System.IO
+
+open Generators
 
 let generate bound outDir =
-    ignore 0
+    if not(Directory.Exists(outDir)) then invalidArg "outDir" "Directory does not exist"
+    printfn "Generating data with upper bound %i to %s" bound outDir
+
+    let generateFile file (writer: TextWriter -> unit) =
+        using(new FileStream(Path.Combine(outDir, file), FileMode.Create, FileAccess.Write)) (fun fs ->
+            using(new StreamWriter(fs)) writer
+        )
+
+    let generateFileByLine file writer =
+        generateFile file (fun sw -> writer (fun (s1: string, s2: string) -> sw.Write(s1); sw.Write('\t'); sw.WriteLine(s2)))
+
+    let generateFileGuidToByte file writer =
+        generateFileByLine file (fun w -> writer (fun (id: Guid) (t: byte) -> w(id.ToString("N"), t.ToString("D3"))))
+
+    let sw = System.Diagnostics.Stopwatch();
+    let st =
+        sw.Start
+    let ts s =
+        sw.Stop()
+        printfn "%s in %s" s (sw.Elapsed.ToString "mm\:ss\.fff")
+        sw.Reset()
+
+    st()
+    let ants = generateAnts bound ("ants" |> generateFileGuidToByte)
+    ts "Ants generated"
+    st()
+    let cells = generateCells bound ("cells" |> generateFileGuidToByte)
+    ts "Cells generated"
+    st()
+    generateFileByLine "antsToCells" (fun w -> 
+        ants
+        |> Seq.collect (fun ant -> Seq.init (rand.Next(1, 3)) (fun _ -> (ant, cells.[rand.Next(bound)])))
+        |> Seq.take bound
+        |> Seq.sortBy snd
+        |> Seq.iter (fun (ant, cell) -> w (ant.ToString("N"), cell.ToString("N")))
+    )
+    ts "Links generated"
 
 [<EntryPoint>]
 let main argv = 
@@ -13,7 +52,7 @@ let main argv =
         | [|v|] ->
             match Int32.TryParse v with
             | (true, bound) -> generate bound defOut
-            | (false, dir) -> generate defBound dir
+            | (false, _) -> generate defBound v
         | [|v;dir|] -> generate (Int32.Parse v) dir
         | _ -> invalidArg "argv" "Too many arguments"
     with
