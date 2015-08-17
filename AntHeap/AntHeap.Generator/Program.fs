3b0f64764b6cc9fe6e2ef119b3fb7ab0b44c677f
Serg Rogovtsev <lair@hawke.ru>
2015-08-17 21:14:07 +0300

    Generator boilerplate

---
+++ AntHeap/AntHeap.Generator/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+
+let generate bound outDir =
+    ignore 0
+
+[<EntryPoint>]
+let main argv = 
+    try
+        let defBound = 1000
+        let defOut = System.Environment.CurrentDirectory
+        match argv with
+        | [||] -> generate defBound defOut
+        | [|v|] ->
+            match Int32.TryParse v with
+            | (true, bound) -> generate bound defOut
+            | (false, dir) -> generate defBound dir
+        | [|v;dir|] -> generate (Int32.Parse v) dir
+        | _ -> invalidArg "argv" "Too many arguments"
+    with
+    | e -> printfn "%A" e
+    0
