ff30184dcaea25522a9fb5859f812273b8c6e603
Serg Rogovtsev <lair@hawke.ru>
2016-08-05 01:33:41 +0300

    Testing Sorted Multi Array Struct performance
    https://habrahabr.ru/post/306898/

---
+++ SMAS/ARRAY.cs
@@ -0,0 +1,153 @@
+﻿using System;
+
+namespace SMAS
+{
+    class ARRAY
+    {
+        private ARRAY nextLevel = null;
+        int[] keyResult = null;
+		int[] keyLeft = null;
+        int[] keyRight = null;
+        int[] keyTemp = null;
+        int countResult = 0;
+        int countLeft = 0;
+        int countRight = 0;
+        int len = 0;
+
+        int upd() //пошаговое отложенное слияние двух массивов в один
+        {
+            var ops = 0;
+            if (keyResult != null)
+            {
+                if (countLeft < len)
+                {
+                    if (countRight < len)
+                    {
+                        if (keyLeft[countLeft] < keyRight[countRight]) //если текущий элемент 1-го подмассива меньше 2-го - вносим его
+                        {
+                            keyResult[countResult] = keyLeft[countLeft];
+                            countResult++;
+                            countLeft++;
+                            ops++;
+                        }
+                        else //если текущий элемент 2-го подмассива меньше 1-го - вносим его в массив слияния
+                        {
+                            keyResult[countResult] = keyRight[countRight];
+                            countResult++;
+                            countRight++;
+                            ops++;
+                        }
+                    }
+                    else  //значит 2-й массив уже весь перемещён и необходимо добавить сл. элемент из первого подмассива
+                    {
+                        keyResult[countResult] = keyLeft[countLeft];
+                        countResult++;
+                        countLeft++;
+                        ops++;
+                    }
+                }
+                else
+                {
+                    if (countRight < len)   //значит 1-й массив уже весь перемещён - добавляем сл. элемент из второго подмассива
+                    {
+                        keyResult[countResult] = keyRight[countRight];
+                        countResult++;
+                        countRight++;
+                        ops++;
+                    }
+                    //в противном случае все элементы обоих массивов уже присутствуют в результирующем - ничего не делаем
+                }
+
+                if (countResult == len * 2)   //Если после очередного шага слияние завершилось
+                {
+                    if (nextLevel == null) nextLevel = new ARRAY();  //Создаём сл. уровень, если он ещё не существует
+                    ops += nextLevel.set(keyResult, len * 2);
+                    keyResult = null;
+                    keyLeft = null;
+                    keyRight = null;
+                    keyTemp = null;
+                    ops++;
+                }
+            }
+
+            ops += nextLevel?.upd() ?? 0;
+            return ops;
+        }
+
+        int set(int[] Key, int Len)
+        {
+            var ops = upd();
+            len = Len;
+            if (keyTemp == null)
+            {
+                keyTemp = Key;
+                ops++;
+            }
+            else
+            {
+                keyLeft = Key;
+                ops++;
+                keyRight = keyTemp;
+                ops++;
+                keyTemp = null;
+                countResult = 0;
+                countLeft = 0;
+                countRight = 0;
+                keyResult = new int[len * 2];
+            }
+            ops += upd();
+            return ops;
+        }
+
+        public int set(int Key)
+        {
+            SetCount++;
+            return set(new [] {Key}, 1);
+        }
+
+        public int SetCount { get; private set; } = 0;
+
+        public bool get(int Key)
+        {
+            int l, r, i;
+            if (keyTemp != null)
+            {
+                l = 0; r = len;
+                while (l != r - 1)
+                {
+                    i = (l + r) / 2;
+                    if (keyTemp[i] < Key) l = i;
+                    else if (keyTemp[i] > Key) r = i;
+                    else return true;
+                }
+                if (keyTemp[l] == Key) return true;
+            }
+            if (keyLeft != null)
+            {
+                l = 0; r = len;
+                while (l != r - 1)
+                {
+                    i = (l + r) / 2;
+                    if (keyLeft[i] < Key) l = i;
+                    else if (keyLeft[i] > Key) r = i;
+                    else return true;
+                }
+                if (keyLeft[l] == Key) return true;
+            }
+            if (keyRight != null)
+            {
+                l = 0; r = len;
+                while (l != r - 1)
+                {
+                    i = (l + r) / 2;
+                    if (keyRight[i] < Key) l = i;
+                    else if (keyRight[i] > Key) r = i;
+                    else return true;
+                }
+                if (keyRight[l] == Key) return true;
+            }
+
+            return nextLevel?.get(Key) ?? false;
+        }
+    };
+}
---
+++ SMAS/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace SMAS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new ARRAY();
+            var reference = new HashSet<int>();
+            var rnd = new Random();
+
+            reference.Add(1);
+            
+            var ops = test.set(1);
+            ops += test.set(1);
+            for (var i = 0; i <= 100000; i++)
+            {
+                var x = rnd.Next();
+                ops += test.set(x);
+                if (i % 10000 == 0)
+                    Console.WriteLine($"For {test.SetCount} sets {ops} operations performed");
+
+                reference.Add(x);
+                Debug.Assert(test.get(x));
+            }
+
+            for (var i = 0; i <= 1000000; i++)
+            {
+                var x = rnd.Next();
+                var actual = test.get(x);
+                var expected = reference.Contains(x);
+                Debug.Assert(actual == expected, $"Expected {expected}, got {actual}");
+            }
+        }
+    }
+}
